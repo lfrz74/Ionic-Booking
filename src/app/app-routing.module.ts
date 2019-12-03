d2d1f8071545e6db6e8c144f7ff4fddedfda414e
lfrz <luisfernandoriverazapata@gmail.com>
2019-12-03 10:55:18 -0500

    Fernando Rivera Creación de proyecto 03/Dic/2019

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { BookingsPageModule } from './bookings/bookings.module';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'places', pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 'booking',
+    loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
+  },
+  {
+    path: 'places',
+    loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
+  }/*,
+  {
+    path: 'discover',
+    loadChildren: () => import('./places/discover/discover.module').then( m => m.DiscoverPageModule)
+  },
+  */
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
