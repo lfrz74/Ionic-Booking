939326eb78971cee16a5a7ab2274d3711e94963f
lfrz <luisfernandoriverazapata@gmail.com>
2019-12-20 22:22:36 -0500

    20/Dic/2019 Spinner vs Loading Controller

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-auth',
@@ -12,8 +13,10 @@ export class AuthPage implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private router: Router
-    ) { }
+    private router: Router,
+    private loadingCtrl: LoadingController) {
+
+    }
 
   ngOnInit() {
   }
@@ -21,9 +24,15 @@ export class AuthPage implements OnInit {
   onLogin() {
     this.isLoading = true;
     this.authService.login();
-    setTimeout(() => {
-      this.isLoading = false;
-      this.router.navigateByUrl('/places/tabs/discover');
-    }, 2000);
+    this.loadingCtrl
+      .create({ keyboardClose: true, message: 'Logging in...' })
+      .then(loadingEl => {
+        loadingEl.present();
+        setTimeout(() => {
+          this.isLoading = false;
+          loadingEl.dismiss();
+          this.router.navigateByUrl('/places/tabs/discover');
+        }, 2000);
+      });
   }
 }
--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, ModalController } from '@ionic/angular';
+import { NavController, ModalController, ActionSheetController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 import { PlacesService } from '../../places.service';
 import { Place } from '../../places.model';
@@ -17,7 +17,8 @@ export class PlaceDetailPage implements OnInit {
       private navCtrl: NavController,
       private route: ActivatedRoute,
       private placesService: PlacesService,
-      private modalCtrl: ModalController
+      private modalCtrl: ModalController,
+      private actionSheetCtrl: ActionSheetController
     ) { }
 
   ngOnInit() {
@@ -31,7 +32,35 @@ export class PlaceDetailPage implements OnInit {
   }
 
   onBookPlace() {
-    // this.navCtrl.navigateBack('/places/tabs/discover');
+    this.actionSheetCtrl.create({
+      header: 'Choose an Action',
+      buttons: [
+        {
+          text: 'Select Date',
+          handler: () => {
+            this.openBookingModal('select');
+
+          }
+        },
+        {
+          text: 'Random Date',
+          handler: () => {
+            this.openBookingModal('random');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    }).then(actionSheetEl => {
+      actionSheetEl.present();
+    });
+
+  }
+
+  openBookingModal(mode: 'select' | 'random') {
+    console.log(mode);
     this.modalCtrl.create({
       component: CreateBookingComponent,
       componentProps: { selectedPlace: this.place },
