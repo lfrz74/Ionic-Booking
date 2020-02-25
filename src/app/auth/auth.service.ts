3f9013ce24376113d5ca419ab039e1895fb711b6
lfrz74 <anonimus47@hotmail.com>
2020-02-24 20:56:07 -0500

    24/Feb/2020

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean
+}
+
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +20,7 @@ export class AuthService {
   // tslint:disable-next-line: variable-name
   private _userIsAuthenticated = false;
   // tslint:disable-next-line: variable-name
-  private _userId = 'def';
+  private _userId = null;
 
   get userIsAuthenticated() {
     return this._userIsAuthenticated;
@@ -17,10 +30,20 @@ export class AuthService {
     return this._userId;
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
-  login() {
-    this._userIsAuthenticated = true;
+  signup(mail: string, pwd: string) {
+    return this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`,
+    {email: mail,
+     password: pwd,
+     returnSecureToken: true
+    });
+  }
+
+  login(mail: string, pwd: string) {
+    return this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
+    {email: mail, password: pwd});
   }
 
   logout() {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  googleMapsAPIKey: ''
+  googleMapsAPIKey: '',
+  firebaseAPIKey: ''
 };
